d0cbc4d95315f47f540b2fc99fcce234706de907
IBRA67313 <ifradiakite1@gmail.com>
2024-12-30 20:23:52 +0100

    Add files via upload

---
+++ Malflix.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-TV</title>
+    <link rel="stylesheet" href="Malflix.css">
+</head>
+<body id="home">
+<nav>
+    <img id="logo" src="2024_07_11_15_11_IMG_5802.png"; alt="">
+</nav>
+<div class="content">
+
+<form action="Profil.php">
+    <h2>S'indentifier<h2>
+    <input type="email" placeholder="Entrez votre Email"><br>
+    <input type="password" placeholder="Entrez votre mot de passe"><br>
+    <input type="submit" value="connexion"><br>
+    <p> Vous n'avez pas encore de compte ?</p>
+    <div align="center" class="Creer un compte"> 
+    <a href="Inscription.php">Créer un compte</a> 
+    </div>
+</form>
+</div>
+</body>
+</html>
